0fffab8f1c1c7c2ac887b98d56a2b1f0a631bf73
Antonio Cortés <antonio.cortes@es.unisys.com>
2014-11-07 12:52:18 +0100

    Header changes

--- index.js
+++ index.js
@@ -12,8 +12,22 @@ app.get('/', function(req, res){
 });
 
 //Middleware
+
+// Enable Cross-Origin-Access
+app.use('*', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+ });
+
 // We are going to protect / routes with JWT
 app.use('/api', expressJwt({secret: 'tokenSecret'}));
+app.use(function (err, req, res, next) {
+  if (err.name === 'UnauthorizedError') {
+    console.log('UnauthorizedError');
+    res.status(401).send('Invalid token...');
+  }
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(function (req, res, next) {
